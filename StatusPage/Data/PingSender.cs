56d1cb78e7f000d593fb5b1b95fc259b2d8878cb
Laveder Mark-Ylann <mark-ylann.laveder@etu.umontpellier.fr>
2021-11-15 22:56:15 +0100

    Ajoutez des fichiers projet.

---
+++ StatusPage/Data/PingSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace StatusPage.Data
+{
+    public class PingSender
+    {
+        // Domaine ou IP
+        public string Adresse;
+        public PingReply Reponse;
+
+    }
+}
---
+++ StatusPage/Data/PingSenderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatusPage.Data
+{
+    public class PingSenderService
+    {
+        public string[] Adresses = ["https://www.google.com", "www.facebook.com", "92.123.109.47", "2a02:26f0:129:389::1aca"];
+
+        public Task<PingSender[]> GetPingAsync(DateTime startDate)
+        {
+            return Task.FromResult(Enumerable.Range(0, Adresses.Length).Select(index => new PingSender
+            {
+                Adresse = Adresses[index]
+            }).ToArray());
+        }
+    }
+}
